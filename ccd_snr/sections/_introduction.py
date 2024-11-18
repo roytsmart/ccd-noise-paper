f9915016be389b4f4fe7e0b5d130fcf7ab8f227f
Roy Smart <roytsmart@gmail.com>
2024-11-18 00:56:44 -0700

    Updates to intro

--- ccd_snr/sections/_introduction.py
+++ ccd_snr/sections/_introduction.py
@@ -11,7 +11,7 @@ and are currently used in many of NASA's most ambitious solar missions,
 such as \AIA\ \citep{Lemen2012} and \IRIS\ \citep{DePontieu2014}.
 
 Despite their popularity,
-understanding the \QE\ and  noise statistics of these sensors in the \UV\
+understanding the \QE\ and noise statistics of these sensors in the \UV\
 is challenging due to the phenomenon of \PCC,
 where photoelectrons generated near the back surface of the sensor
 (due to the shallow penetration depth of \UV\ photons)
@@ -28,7 +28,7 @@ Another source of noise which is even less understood than \PCC\ is
 Fano noise \citep{Fano1947}, 
 the unavoidable variation in the number of electrons measured per photon.
 Fano noise is routinely measured in soft X-ray regime \citep{Rodrigues2023},
-but has possibly never been measured in the \UV\ despite being predicted to
+but is rarely measured in the \UV\ despite being predicted to
 have significant width \citep{Santos1991} and skew \citep{Fraser1994}
 variations as a function of wavelength.
 Since the Fano noise in silicon is so small,
@@ -37,13 +37,14 @@ which still exhibits the width variations observed in \citet{Santos1991}
 since \UV\ photons produce very few photoelectrons.
 
 We will use these two noise sources along with a simple absorbance calculation
-to model a silicon imaging sensor and to develop an analytic
-expression for the expected noise measured by this sensor as a function of
-the mean signal.
-This model will be applied to predict the noise measured by some typical solar
-\UV\ instruments,
-and we will also compare our results to some unexpected noise measurements in
-\IRIS\ \citep{Wulser2018}.
+to model a silicon imaging sensor and estimate the amount of noise as a function
+of wavelength.
+This model will be used to demonstrate the importance of recombination by
+comparing it to a model which considers only photon shot noise.
+We will then evaluate this model for several popular solar \UV\ instruments
+and predict the amount of noise for the nominal wavelengths of each instrument.
+Finally, we will possibly resolve a discrepancy described in \citep{Wulser2018}
+regarding the slope of the photon transfer curve for the \IRIS\ instrument.
 """
     )
     return result
