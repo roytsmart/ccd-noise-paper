aba120f639e2668c40ff7b54a2b9ec71d1a9ab61
Roy Smart <roytsmart@gmail.com>
2024-11-19 21:43:56 -0700

    fixes

--- ccd_snr/sections/__init__.py
+++ ccd_snr/sections/__init__.py
@@ -1,6 +1,6 @@
-from ._abstract import abstract
-from ._introduction import introduction
-from ._model import model
+from ._s0_abstract import abstract
+from ._s1_introduction import introduction
+from ._s2_model import model
 
 __all__ = [
     "abstract",
