ae272bacfda973291c8cde3c1fb687959e438a19
Roy Smart <roytsmart@gmail.com>
2025-01-02 10:26:16 -0700

    Lots of changes to Discussion section

--- ccd_snr/sections/_s3_discussion.py
+++ ccd_snr/sections/_s3_discussion.py
@@ -5,28 +5,60 @@ import ccd_snr
 def discussion() -> aastex.Section:
     result = aastex.Section("Results and Discussion")
     result.append(
-        r"""
+        r"""     
 Since $\text{EQE}(\lambda)$ is a type of efficiency,
-it's tempting to think that the variance of the signal measured by a silicon 
-sensor is simply proportional to the product 
-$\text{EQE}(\lambda) \times \langle N_\gamma \rangle$,
-which we've plotted in Figures \ref{fig:photonNoise} and \ref{fig:electronNoise}
-labeled as the ``naive'' model.
-In most of the \SXR\ and visible wavelengths,
-the naive model is a good approximation of the noise model developed in this work.
-However, in the \UV\ wavelengths the naive model overestimates the variance
-of the noise by up to a factor of $\sim 2$ since the binomial distribution
-is much narrower than the Poisson distribution when the \CCE\ is low.
+it's tempting to treat the sensor like any other component in a given optical system
+and simply use $\text{EQE}(\lambda)$ as another factor in the effective
+area calculation.
+This simple noise model is formalized in Equations 9 and 10 of \citet{Stern1986},
+which gives the \VSR\ of the number of measured electrons as
+\begin{equation} \label{eq:sternVSR}
+    \text{VSR}(N_e'') = \text{IQY}(\lambda) + \mathcal{F} \quad \text{(incomplete!).}
+\end{equation}
+This is presumably the noise model used by most solar instrument teams.
+For comparison, the \VSR\ predicted by our noise model is
+\begin{equation} \label{eq:ourVSR}
+    \text{VSR}(N_e'') = \left( \text{IQY}(\lambda) + \mathcal{F}_a - 1 \right) \text{CCE}(\lambda) + 1,
+\end{equation}
+where the apparent Fano factor,
+\begin{equation}
+    \mathcal{F}_a = \mathcal{F} + \frac{1/6}{\text{IQY}(\lambda)},
+\end{equation}
+accounts for additional noise due to electron discretization effects.
+In Figures~\ref{fig:photonNoise} and~\ref{fig:electronNoise},
+we've compared Equation~\ref{eq:sternVSR} (gray) 
+to compare our model, Equation~\ref{eq:ourVSR} (black). 
+Over most of the \SXR\ and visible wavelengths,
+The \citet{Stern1986} model is a good approximation of the noise 
+model developed in this work.
+However, the effect of \PCC\ violates the assumptions of \citet{Stern1986}
+and Equation~\ref{eq:sternVSR} overestimates the variance
+predicted by our model by up to a factor of ${\sim}2$ in the \UV\ since the 
+binomial distribution is narrower than the equivalent Poisson distribution,
+especially as $\text{IQY}(\lambda)$ (the number of trials) approaches unity.
 This is good news for engineers building \UV\ astronomical instruments since
-there is much less noise than expected from the naive model in this wavelength
-range.
-
+there is much less noise than expected from the \citet{Stern1986} model in this 
+wavelength range."""
+    )
+    result.append(ccd_snr.tables.fano_factor())
+    result.append(
+        r"""
 In Table~\ref{table:instrumentVSR},
 we've calculated the \VSR\ in terms of incident photons and measured electrons
 for the target wavelengths of a few popular and upcoming solar instruments:
-\AIA, \IRIS, and \MUSE \citep{DePontieu2020}.
+\AIA, \IRIS, and \MUSE\ \citep{DePontieu2020},
 The results show that for the \EUV\ channels, \AIA\ and \MUSE\ are nearly
-shot-noise-limited since the \VSR\ in units of incident photons is near unity.
+shot noise limited since the \VSR\ in units of incident photons is near unity.
+
+Plotted in the right column of Table~\ref{table:instrumentVSR} is
+the improvement factor between the \VSR\ of the \citet{Stern1986} noise model
+and our noise model.
+These ratios show that the \citet{Stern1986} model overestimates the variance
+in the \AIA\ and \IRIS\ \FUV\ channels by about 50\%.
+The \citet{Stern1986} model overestimates the variance even more
+in the short-wavelength \EUV\ channels of \AIA\ and \MUSE.
+Note that since the improvement factor is in variance units,
+the improvement in \SNR\ is the square root of the improvement factor.
 
 Table~\ref{table:instrumentVSR} also partially resolves a discrepancy
 in the theoretical vs. measured noise in \IRIS.
@@ -38,18 +70,9 @@ which is much closer to the measured value.
 The remaining discrepancy may be due to charge diffusion as suggested by
 \citet{Wulser2018}.
 
-Taken together, Equation~\ref{eq:quantum-efficiency},
-Algorithm~\ref{alg:electron-sample}, 
-and Equation~\ref{eq:chargeDiffusionWidth} are enough to fully-describe our 
-model of the sensor.
-Algorithm~\ref{alg:electron-sample} samples the distribution of the number
-of measured electrons given the expected number of incident photons.
-Equation~\ref{eq:chargeDiffusionWidth} defines the spatial resolution of the
-sensor,
-and Equation~\ref{eq:quantum-efficiency} is used to invert 
-Algorithm~\ref{alg:electron-sample} and compute the expected number of 
-incident photons given the number of measured electrons.
+In retrospect, if we had grouped IQY and CCE together into an effecdtive QY, instead of A and CCE,
+this confusion may not have happened.
+ADD FIGURE SUPPORTING THIS.
 """
     )
-    result.append(ccd_snr.tables.fano_factor())
     return result
