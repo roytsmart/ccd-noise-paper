b761df9a60f2ce93b01842b216ddbe3a4ab479d2
Roy Smart <roytsmart@gmail.com>
2025-01-13 21:05:25 -0700

    Updates to abstract

--- ccd_snr/sections/_s0_abstract.py
+++ ccd_snr/sections/_s0_abstract.py
@@ -9,23 +9,22 @@ def abstract() -> aastex.Abstract:
     result = aastex.Abstract()
     result.append(
         r"""
-Silicon-based imaging sensors are a critical component for solar \UV\ astronomy.
+Silicon-based imaging sensors are a critical component for \UV\ astronomy.
 Their high sensitivity and low noise are a vital part of making solar
 \UV\ telescopes practical to build.
 However, \UV\ light is unique compared to other components of the
 electromagnetic spectrum since it has a shallow penetration depth
-into the silicon substrate, which means that the corresponding electron-hole pairs
-have a significant chance of recombination before being measured by the sensor.
-In this article, we will estimate the noise measured by the theoretical silicon
-sensor described in \citet{Stern1994} and show that modeling the recombination 
-process is an important component of the expected total noise.
-We will also introduce an easy-to-implement algorithm which can draw samples 
-from the distribution of electrons measured for a given number of incident photons,
-valid over the entire wavelength range of silicon sensors (0.1-1000 nm).
-Finally, we will apply this model to the \AIA, \IRIS, and \MUSE\ instruments and 
-calculate the expected slopes of the photon transfer curves for a few
-wavelengths important to each instrument.
-\acresetall
+into the silicon substrate, where the corresponding electron-hole pairs
+have a significant chance of recombination before being measured.
+In this article, we will use the theoretical sensor described in \citet{Stern1994}
+as the basis for a noise model which accounts for the effect of recombination,
+valid from the soft X-ray to the near-infrared (0.1-1000 nm).
+Using this model, we will show that considering the effect of recombination
+improves the predicted \SNR\ of a silicon sensor in the \UV\ by up to $\sqrt{2}$
+compared to a model which does not consider recombination.
+We will evaluate this model for the \AIA, \IRIS, and \MUSE\ solar instruments and find that, 
+our model predicts a significant improvement in the expected \SNR\ of some 
+channels of each instrument.
 """
     )
     return result
