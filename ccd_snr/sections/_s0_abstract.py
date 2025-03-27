33ea359b3d63c9d1b6cab754490565f055952195
Roy Smart <roytsmart@gmail.com>
2025-03-27 16:39:10 -0600

    abstract updates

--- ccd_snr/sections/_s0_abstract.py
+++ ccd_snr/sections/_s0_abstract.py
@@ -14,20 +14,23 @@ Their high sensitivity and low noise are a vital part of making
 \UV\ telescopes practical to build.
 However, \UV\ light is unique compared to other components of the
 electromagnetic spectrum since it has a shallow penetration depth
-into the silicon substrate, where the electron-hole pairs corresponding to an absorbed photon
-have a significant chance of recombination before being measured.
-In this article, we will use the theoretical sensor described in \citet{Stern1994}
-as the basis for a noise model which accounts for the effect of recombination,
+into the silicon substrate and is absorbed in a region where the corresponding 
+electron-hole pairs have a significant chance of recombination before being measured.
+This phenomenon is known as \PCC, 
+and in this article we will use the theoretical 
+sensor described in \citet{Stern1994}
+as the basis for a noise model which accounts for \PCC\ and is
 valid from the soft X-ray to the near-infrared (0.1-1000 nm).
-Using this model, we will show that considering the effect of recombination
+Using this model, we will show that considering the effect of \PCC\
 improves the predicted \SNR\ of a silicon sensor in the \UV\ by up to $\sqrt{2}$
 compared to a model which does not consider recombination.
-We will evaluate this model for the \AIA, \IRIS, and \MUSE\ solar instruments and find that, 
+We will evaluate this model for the \AIA, \IRIS, \MUSE, and \WFC\ instruments and find that
 our model predicts a significant improvement in the expected \SNR\ of some 
 channels of each instrument.
 We will also develop a procedure which can efficiently sample the distribution
 of measured electrons and is suitable for use in forward models of astronomical
 instruments.
+\acresetall
 """
     )
     return result
