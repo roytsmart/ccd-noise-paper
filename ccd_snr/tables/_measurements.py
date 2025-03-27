3865bf7181ea1167a23b447e82e20821a8f06fee
Roy Smart <roytsmart@gmail.com>
2025-03-27 16:55:37 -0600

    black

--- ccd_snr/tables/_measurements.py
+++ ccd_snr/tables/_measurements.py
@@ -14,11 +14,12 @@ def measurements() -> str:
     index_uv = dict(wavelength=slice(None, ~0))
     index_visible = dict(wavelength=~0)
 
+    wavelength_iris = [
+        1350 * u.AA,
+        4500 * u.AA,
+    ]
     wavelength_iris = na.ScalarArray(
-        ndarray=u.Quantity([
-            1350 * u.AA,
-            4500 * u.AA,
-        ]),
+        ndarray=u.Quantity(wavelength_iris),
         axes="wavelength",
     )
     wavelength_wfc3 = ccd_snr.instruments.wfc3.wavelength
@@ -32,15 +33,18 @@ def measurements() -> str:
     ratio_model_iris = ratio_model_iris[index_uv]
     ratio_model_wfc3 = ratio_model_wfc3[index_uv]
 
-    ratio_measured_iris = np.array([
+    ratio_measured_iris = [
         1.5,
-    ])
-    ratio_measured_wfc3 = np.array([
+    ]
+    ratio_measured_wfc3 = [
         1.07,
         1.08,
         1.04,
         1.00,
-    ])
+    ]
+
+    ratio_measured_iris = np.array(ratio_measured_iris)
+    ratio_measured_wfc3 = np.array(ratio_measured_wfc3)
 
     ratio_measured_iris = na.ScalarArray(ratio_measured_iris, axes="wavelength")
     ratio_measured_wfc3 = na.ScalarArray(ratio_measured_wfc3, axes="wavelength")
