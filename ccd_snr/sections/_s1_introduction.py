c9c69632fdfba4f5ba7fabf48d80f7d838602adb
Roy Smart <roytsmart@gmail.com>
2025-09-30 08:57:35 -0600

    Even more updates to the intro

--- ccd_snr/sections/_s1_introduction.py
+++ ccd_snr/sections/_s1_introduction.py
@@ -5,44 +5,52 @@ def introduction() -> aastex.Section:
     result = aastex.Section("Introduction")
     result.append(
         r"""
-Backilluminated, silicon-based image sensors such as \CCDs\ and \CMOS\ sensors 
+Back-illuminated, silicon-based image sensors such as \CCDs\ and \CMOS\ sensors 
 are ubiquitous in \UV\ astronomy, 
 and are currently used in many of the community's most ambitious missions,
 such as \AIA\ \citep{Lemen2012}, \IRIS\ \citep{DePontieu2014},
-and \WFC\ \citep{Kimble2008} on the HST.
-Predicting the noise introduced by these sensors is important for determining the
-performance of future astronomical instruments and to examine if a given
-measurement is feasible.
-Understanding this noise is also useful for characterizing current astronomical
-observations since it allows for better quantification of uncertainties for
-wavelengths where a detailed noise measurement is not available.
+and \WFC\ \citep{Kimble2008} on HST.
+Predicting the noise measured by these sensors is important both
+for quantifying the uncertainty on current astronomical observations
+as well as for evaluating the performance of proposed future instruments.
 
-Often the largest source of noise on images captured by these sensors is 
+Often the largest source of noise measured by these sensors is 
 shot noise \citep{Stern1986}.
 This noise is due to the quantized nature of light \citep{Schottky1918}
 and is not a property of the sensor \textit{per se},
-but the fraction of incident light that interacts with the sensor determines 
-the magnitude of the shot noise.
-Another well-known source of noise inherent to silicon-based sensors is 
+but its magnitude depends on the number of interacting photons,
+which is determined by the sensor's geometry and optical constants.
+Another well-known source of noise inherent to silicon sensors is 
 Fano noise \citep{Fano1947},
-the unavoidable variation of the number of electrons produced
+the random variation of the number of electrons produced
 per interacting photon.
 This noise source is small \citep{Rodrigues2023},
 and often ignored entirely,
-but it is usually combined with the shot noise to estimate the total noise
-measured by these sensors \citep{Stern1986,Janesick2001}.
+but it is usually combined with the shot noise to form a traditional estimate 
+of the total noise \citep{Stern1986,Janesick2001}.
 
-This traditional model of shot noise and Fano noise is sufficient in X-ray 
-wavelengths \cite{Athiray2020},
-and in visible light wavelengths where there is no Fano noise.
-However, in \citet{Wulser2018} they showed that \IRIS\ measured \textit{less} noise
-in the \FUV\ than the traditional model would suggest and
-similar results were found in the \NUV\ by \WFC\ \citep{Marinelli2024}.
-These results show that the traditional model is incomplete and that a more
-complicated model is needed to fully explain the observed noise statistics.
+This traditional model of shot noise and Fano noise is very accurate in the
+visible \citep{Marinelli2024} and X-ray \citep{Ishikawa2011,Athiray2020,Schwab2020}
+wavelength regimes.
+However, this model appears to be less accurate in the \UV.
+For example, \citet{Wulser2018} showed that \IRIS\ measured \textit{less} noise
+in the \FUV\ channel than the traditional model would suggest
+and similar results were found in the \NUV\ by \WFC\ \citep{Marinelli2024}.
+This discrepancy indicates that the traditional model is incomplete, 
+and that a more detailed model is needed to predict the noise measured by 
+these sensors in the \UV.
 
-In this work, we will propose a model where the phenomenon of \PCC\ accounts
-for this observed discrepancy.
+In this work, we aim to resolve this discrepancy by developing a noise model
+which adds two additional effects to the traditional model.
+First, we examine \PCC,
+which occurs when electron-hole pairs recombine before they can be measured
+by the sensor \citep{Stern1994,Janesick2001}.
+Second, we investigate the role of charge diffusion (or charge spreading),
+the tendency for photoelectrons to migrate into neighboring pixels \citep{Janesick2001},
+a mechanism suggested by both \citet{Wulser2018} and \citet{Marinelli2024} to
+resolve this discrepancy.
+We will also provide a reference implementation of our model in Python and use
+it to predict the noise measured by \AIA, \IRIS, \MUSE, and \WFC.
 """
     )
     return result
